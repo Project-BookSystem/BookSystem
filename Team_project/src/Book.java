584dffe4c0a42ad9333abf7cf0eed78873a5aba6
805-09 <805-09@DESKTOP-G8AKUCT>
2025-02-13 17:31:44 +0900

    25021317

--- Team_project/src/Book.java
+++ Team_project/src/Book.java
@@ -1,4 +1,8 @@
 
 public class Book {
-
+	
+	
+	public void control() {
+		System.out.println("작업선택 [c:도서등록, u:도서정보수정, r:]");
+	}
 }
--- Team_project/src/Main.java
+++ Team_project/src/Main.java
@@ -4,17 +4,17 @@ public class Main {
 	
 	static Scanner s = new Scanner(System.in);
 	
-
 	public static void main(String[] args) {
 		Book book= new Book();
 		Member member = new Member();
 		Rent rent = new Rent();
 		
 		while(true) {
-			System.out.println("작업을 선택해 주세요. [B: 도서 관리, M: 회원 관리, R: 대여 관리, X: 프로그램 종료]");
+			System.out.print("작업을 선택해 주세요. [B:도서 관리, M:회원 관리, R:대여 관리, X:프로그램 종료] : ");
 			String admin = s.nextLine();
 			
 			if (admin.equals("x") || admin.equals("X")) break;
+			
 			switch(admin) {
 			case "b" , "B" :
 				//book.control();
@@ -24,8 +24,15 @@ public class Main {
 				break;
 			case "r" , "R" :
 				//rent.control();
+			default:
+				System.out.println("잘못된 입력입니다.\n");
 			}
 		}
 	}
+	
+//	public static boolean isNumber(String str) {
+//		// 정규식을 사용하여 숫자와 소수점만 있는지 확인
+//		return str.matches("-?\\d*(\\.\\d+)?"); // 정규표현식
+//	}
 
 }
