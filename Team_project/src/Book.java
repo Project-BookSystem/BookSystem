cac00975af0dc33dacf45c85c3940e8c62b24fd1
805-09 <805-09@DESKTOP-G8AKUCT>
2025-02-13 17:36:59 +0900

    25021317

--- Team_project/src/Book.java
+++ Team_project/src/Book.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Book {
 	
+	Scanner ss = new Scanner(System.in);
+	Scanner si = new Scanner(System.in);
 	
 	public void control() {
-		System.out.println("작업선택 [c:도서등록, u:도서정보수정, r:]");
+		System.out.println("[도서 관리]");
+		System.out.println("작업선택 [c:도서등록, u:도서정보수정, r:도서목록조회, d:도서삭제] : ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
