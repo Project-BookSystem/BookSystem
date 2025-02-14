4c9988244aadaf316f9efc61b382211d4dab30bf
805-09 <805-09@DESKTOP-G8AKUCT>
2025-02-14 13:25:22 +0900

    121212121

--- Team_project/src/DBConnection.java
+++ Team_project/src/DBConnection.java
@@ -9,7 +9,6 @@ public class DBConnection {
 	private static final String USER = "Book"; // MySQL 사용자명
 	private static final String PASSWORD = "0212"; // MySQL 비밀번호
 	
-	//abbb
 //	public static Connection getConnection() {
 //		Connection conn = null;
 //		try {
